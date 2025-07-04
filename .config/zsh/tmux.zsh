516d4b60df88a37f67da285dff1929350aafd7fb
aahsnr <ahsanur041@proton.me>
2025-07-04 16:54:12 +0600

    updated

---
+++ .config/zsh/tmux.zsh
@@ -0,0 +1,7 @@
+# tmux aliases
+alias tm='tmux'
+alias tma='tmux attach -t'
+alias tmn='tmux new-session -s'
+alias tml='tmux list-sessions'
+alias tmk='tmux kill-session -t'
+alias tmr='tmux source-file ~/.tmux.conf'
